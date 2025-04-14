a7d11be31dff1648ed78e47c3acb0d8fb6433d5a
Farheen Malik <malikfarheen2002@gmail.com>
2025-04-14 17:56:56 +0500

    Tasks Initial

---
+++ Task1/UsersManagement/src/controller/UserController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express"
+import { AppDataSource } from "../data-source"
+import { User } from "../entity/User"
+
+const userRepo = AppDataSource.getRepository(User)
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    const users = await userRepo.find()
+    res.json(users)
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const user = await userRepo.findOneBy({ id })
+    res.json(user)
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    const newUser = userRepo.create(req.body)
+    const savedUser = await userRepo.save(newUser)
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const user = await userRepo.findOneBy({ id })
+
+    userRepo.merge(user, req.body)
+    const updatedUser = await userRepo.save(user)
+    res.json(updatedUser)
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const user = await userRepo.findOneBy({ id })
+
+    await userRepo.remove(user)
+    res.send("User deleted")
+}
