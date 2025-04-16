102cb35b0d6e061042136596c1b5e4f34000c20d
Farheen Malik <malikfarheen2002@gmail.com>
2025-04-16 13:08:22 +0500

    Email and names validation complete

--- Task1/UsersManagement/src/validation.ts
+++ Task1/UsersManagement/src/validation.ts
@@ -1,23 +1,53 @@
-import { body, query, validationResult } from 'express-validator'
-import { Request, Response } from "express"
+import { body, ExpressValidator, query, validationResult } from 'express-validator'
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
-import {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  getUserByEmail,
-} from "./controller/UserController"
 
 const userRepo = AppDataSource.getRepository(User)
 
 export const checkEmail = [
-  body('email').isEmail().custom(async value => {
+  body('email')
+  .isEmail()
+  .custom(async value => {
     const user = await userRepo.findOneBy({email: value})
     if (user) {
       throw new Error('E-mail already in use');
     }
+    if (/^\d/.test(value))
+    {
+      throw new Error('Email cannot start with a number!');
+    }
+    if (/[A-Z]/.test(value))
+    {
+      throw new Error('Email cannot contain capital letters!');
+    }
+    if (/\s/.test(value))
+    {
+      throw new Error('Email cannot contain spaces!');
+    }
   }),
+]
+
+export const checkfirstName = [
+  body('firstName')
+  .isLength({ min: 3 }).withMessage('First name must be at least 3 characters long')
+  .notEmpty().withMessage('First name is required')
+  .custom(async value => {
+    if (/\s/.test(value)) {
+      throw new Error('First name cannot contain spaces');
+  }
+  else if (/\d/.test(value))
+    {
+      throw new Error('Name cannot contain numbers!');
+    }
+})
+]
+
+export const checkLastName = [
+  body('lastName')
+  .custom(async value => {
+    if (/\d/.test(value))
+    {
+      throw new Error('Name cannot contain numbers!');
+    }
+  })
 ]
