b90033ae5b65fd0bcb6a638199e6e7a3d46f07b2
Farheen Malik <156839406+farheenmalik01@users.noreply.github.com>
2025-04-21 13:18:04 +0500

    today's tasks. jwt, login signup (#3)

--- Task1/UsersManagement/src/controller/UserController.ts
+++ Task1/UsersManagement/src/controller/UserController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express"
 import { AppDataSource } from "../data-source"
 import { User } from "../entity/User"
+import * as bcrypt from 'bcrypt'
+import { validationResult } from "express-validator"
+import jwt from "jsonwebtoken";
+
+const SECRET = "farheen";
+
+const jwt = require("jsonwebtoken");
 
 const userRepo = AppDataSource.getRepository(User)
 
@@ -42,4 +49,71 @@ export const deleteUser = async (req: Request, res: Response) => {
 
     await userRepo.remove(user)
     res.send("User deleted")
+}
+
+const saltRounds = 10
+
+export const signup = async (req: Request, res: Response) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { firstName, lastName, age, email, password } = req.body;
+        
+        const existingUser = await userRepo.findOneBy({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        
+        const newUser = userRepo.create({
+            firstName,
+            lastName,
+            age,
+            email,
+            password: hashedPassword
+        });
+        
+        const savedUser = await userRepo.save(newUser);
+        
+        const { password: _, ...userWithoutPassword } = savedUser;
+        res.status(201).json(userWithoutPassword);
+        
+    } catch (error) {
+        console.error('Signup error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const signin = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userRepo.findOneBy({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const { password: _, ...userWithoutPassword } = user;
+        
+        const token = jwt.sign({ id: user.id }, SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({
+            message: 'Signin successful', token,
+            user: userWithoutPassword
+        });
+
+
+    } catch (error) {
+        console.error('Signin error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
--- Task1/UsersManagement/src/validation.ts
+++ Task1/UsersManagement/src/validation.ts
@@ -4,6 +4,36 @@ import { User } from "./entity/User"
 
 const userRepo = AppDataSource.getRepository(User)
 
+export const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
+export const checkPassword = [
+  body('password')
+      .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+      .matches(/\d/).withMessage('Password must contain a number')
+      .custom(async value => {
+      if (/\s/.test(value))
+        {
+          throw new Error('Password cannot contain spaces!');
+        }
+    }),
+]
+    
+export const checkSignin = [
+  body('email')
+      .isEmail()
+      .withMessage('Please provide a valid email'),
+  body('password')
+      .notEmpty()
+      .withMessage('Password is required'),
+  validate
+];
+
 export const checkEmail = [
   body('email')
   .isEmail()
@@ -50,4 +80,10 @@ export const checkLastName = [
       throw new Error('Name cannot contain numbers!');
     }
   })
+]
+
+
+export const validation = [
+  checkEmail, checkLastName, checkfirstName, checkPassword, validate
+
 ]
