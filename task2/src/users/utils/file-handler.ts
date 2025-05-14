3cc5c3a773ea7dc0855efa9fdee08f22470153d6
Farheen Malik <malikfarheen2002@gmail.com>
2025-05-14 11:26:56 +0500

    Profile Picture added

---
+++ task2/src/users/utils/file-handler.ts
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { User } from '../entities/user.entity';
+
+const dataDir = 'D:/Ideofuzion/task2/src/user-data';
+
+export async function saveUserDataToFile(user: User): Promise<void> {
+  try {
+    console.log('Saving user data to file for user:', user);
+    await fs.mkdir(dataDir, { recursive: true });
+
+    const filePath = join(dataDir, `user-${user.id}.json`);
+    const userData = JSON.stringify(user, null, 2);
+
+    await fs.writeFile(filePath, userData, 'utf-8');
+    console.log(`User data saved to file: ${filePath} for user ID: ${user.id}`);
+  } catch (error) {
+    console.error('Error saving user data to file:', error);
+    throw error;
+  }
+}
